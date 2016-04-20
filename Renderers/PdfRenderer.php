b3807d95ee882dc438d47d14d58768ac1adb959f
Jaroslav Runcik <J.Runcik@seznam.cz>
2016-04-20 21:21:32 +0200

    Czech for PDF

--- Renderers/PdfRenderer.php
+++ Renderers/PdfRenderer.php
@@ -2,7 +2,7 @@
 define('FPDF_FONTPATH','../fpdf/font/');
 require_once('../fpdf/fpdf.php');
 
-class PdfRendererList extends FPDF implements IExportVisitor
+class PdfRenderer extends FPDF implements IExportVisitor
 {
 	public function VisitGaleryBegin()
 	{
@@ -10,8 +10,8 @@ class PdfRendererList extends FPDF implements IExportVisitor
 		$this->kc = iconv("UTF-8", "cp1250", " Kč");
 		$this->odd  = false;
 		
-		$this->AddFont('calibri', '', 'calibri.php');		
-		$this->fontName = 'calibri';
+		$this->AddFont('calibri', '', 'calibri.php');
+		$this->AddFont('calibriB', '', 'calibrib.php');		
 	}
 	
 	public function VisitGalery($galery)
@@ -19,14 +19,11 @@ class PdfRendererList extends FPDF implements IExportVisitor
 		$this->SetFillColor(240, 240, 240);
 		$this->SetTextColor(0, 0, 0);
 		$this->SetLineWidth(0.1);
-		$this->SetFont($this->fontName, '', 16);
+		$this->SetFont('calibri', '', 16);
 		
 		$this->Cell(140, 7, iconv("UTF-8", "cp1250", $galery->GetName()),       'B', 0, 'L');
 		$this->Cell(30, 7, iconv("UTF-8", "cp1250", $galery->GetTotalPrice()) . $this->kc, 'B', 0, 'R');
 		$this->Ln();
-		$this->Ln();
-		
-		$this->SetFont($this->fontName, '', 12);
 	}
 
 	public function VisitGaleryEnd()
@@ -40,8 +37,9 @@ class PdfRendererList extends FPDF implements IExportVisitor
 	
 	public function VisitCustomer($order)
 	{
-		$this->Cell(140, 7, iconv("UTF-8", "cp1250", $order->GetName()),       	  '', 0, 'L', $this->odd);
-		$this->Cell(30, 7, iconv("UTF-8", "cp1250", $order->GetTotalPrice()) . $this->kc, '', 0, 'R', $this->odd);
+		$this->SetFont('calibri', '', 12);
+		$this->Cell(140, 7, iconv("UTF-8", "cp1250", $order->GetName()),       	  'T', 0, 'L');
+		$this->Cell(30, 7, iconv("UTF-8", "cp1250", $order->GetTotalPrice()) . $this->kc, 'T', 0, 'R');
 		$this->Ln();
 		
 		$this->odd = !$this->odd;
@@ -58,6 +56,11 @@ class PdfRendererList extends FPDF implements IExportVisitor
 	
 	public function VisitPhotoDescription($photoDescription)
 	{
+		$this->SetFont('calibriB', '', 10);
+		$this->Cell(5, 7, "");
+		$this->Cell(135, 7, iconv("UTF-8", "WINDOWS-1250", $photoDescription->GetCategory()));
+		$this->Ln();
+		$this->SetFont('calibri', '', 10);
 	}
 
 	public function VisitPhotoDescriptionEnd()
@@ -71,6 +74,10 @@ class PdfRendererList extends FPDF implements IExportVisitor
 	
 	public function VisitPhoto($photo)
 	{
+		$this->Cell(15, 7, "");
+		$this->Cell(105, 7, iconv("UTF-8", "WINDOWS-1250", '- ' . $photo->GetName()));
+		$this->Cell(20, 7, iconv("UTF-8", "WINDOWS-1250", $photo->GetQuantity() . 'x'), '', 0, 'R');
+		$this->Ln();
 	}
 	
 	public function VisitPhotoEnd()
@@ -79,6 +86,5 @@ class PdfRendererList extends FPDF implements IExportVisitor
 	
 	private $kc;
 	private $odd;
-	private $fontName;
 }
 ?>
